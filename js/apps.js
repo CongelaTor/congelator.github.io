eaa16458296aba15182d48705c8972073da33d25
CongelaTor <congelator.contact@gmail.com>
2026-08-12 20:31:53 +0200

    bannieres

--- js/apps.js
+++ js/apps.js
@@ -7,7 +7,7 @@ const applications = [
     status: "Bientôt sur Google Play",
     page: "alto-gaspi.html",
     icon: "img/alto_gaspi/alto_gaspi_512x512.png",
-    hero: "img/alto_gaspi/alto_gaspi_presentation_0x75.jpg",
+    hero: "img/alto_gaspi/alto_gaspi_presentation_1024x500.jpg",
     screenshot: "img/alto_gaspi/alto_gapsi_01.jpg",
     tags: ["Alimentaire", "Anti-gaspillage"]
   },
@@ -19,7 +19,7 @@ const applications = [
     status: "Bientôt sur Google Play",
     page: "flowfy.html",
     icon: "img/flowfy/Flowfy_512x512.png",
-    hero: "img/flowfy/Flowfy_0x75.png",
+    hero: "img/flowfy/Flowfy_1024x500.png",
     screenshot: "img/flowfy/Flowfy_01.jpg",
     tags: ["Organisation", "Productivité"]
   }
